4d84da91507cc0f173c0a26eb114bb427087f25f
Robert-Beane <beane031@morris.umn.edu>
2020-09-02 18:41:52 -0500

    Add function to combine all files into one file to make combing through data easier.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,10 +1,13 @@
 #!/bin/env bash
 
 client=$1 # the client log file
-client_name=$(basename "$client")
 
 # creating the tmp directory that will be working in
 tmp_dir=$(mktemp -d -t processing_log-XXXX)
 
 # extract the log file
 tar -xzf "$client" -C "$tmp_dir"
+
+# combine all secure files together
+cd "$tmp_dir" || exit
+cat -- */*/* > all.txt

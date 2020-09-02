a333992fd9b5c9560b4fc54bd8c3840838197479
Robert-Beane <beane031@morris.umn.edu>
2020-09-02 17:29:28 -0500

    Now properly takes the file and extracts it into a tmp directory.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,10 @@
 #!/bin/env bash
 
 client=$1 # the client log file
+client_name=$(basename "$client")
+
+# creating the tmp directory that will be working in
+tmp_dir=$(mktemp -d -t processing_log-XXXX)
 
 # extract the log file
-tar -xfz "$client"
+tar -xzf "$client" -C "$tmp_dir"

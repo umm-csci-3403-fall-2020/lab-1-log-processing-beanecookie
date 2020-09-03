c5ee973485541c326188ae67a4efb9ab0d673206
Cookie Vang <vang2660@morris.umn.edu>
2020-09-03 10:54:50 -0500

    Sort and count username of failed login

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 client=$1
+cd "$client" || exit
+
+#sort failed login data by username and put into new file
+sort -k 4 -o failed_login_data.txt > sorted_login_data.txt
+
+#count the number of times a username is used
+uniq -c sorted_login_data.txt

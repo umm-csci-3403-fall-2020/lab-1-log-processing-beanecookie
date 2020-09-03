ac6c63f5ba45fe8f693a5aa3ee66bcc6de8014d0
Robert-Beane <beane031@morris.umn.edu>
2020-09-02 19:13:38 -0500

    Add awk statement that properly sorts through the data and gives us the proper columns

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -10,4 +10,7 @@ tar -xzf "$client" -C "$tmp_dir"
 
 # combine all secure files together
 cd "$tmp_dir" || exit
-cat -- */*/* > all.txt
+cat -- * > combined.txt
+
+# find the proper data
+awk -F "[: ]+" '/Failed password for invalid user|Failed password for/{print  $1,$2,$3,$(NF-5),$(NF-3)}' combined.txt > failed_login_data.txt

e31aa2ab178b02f2983dd3005d7f384ba10d04c7
Robert-Beane <beane031@morris.umn.edu>
2020-09-03 21:10:39 -0500

    Attempt fix for create_username_dist.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -10,5 +10,8 @@ awk -F "[: ]+" '{print $4}' sorted_login_data.txt > sorted_user_data.txt
 #count the number of times a username is used
 uniq -c sorted_user_data.txt > counted_user_data.txt
 
+# putting usernames and # of tries into html
+awk -F "[: ]+" '{print  "data.addRow([\x27"$2"\x27, "$1"]);"}' counted_user_data.txt > user_addrow.txt
+
 #wrap the data and create the html file
-#wrap_contents.sh counted_login_data.txt "$client" username_distribution.html
+wrap_contents.sh user_addrow.txt html_components/username_dist username_dist.html

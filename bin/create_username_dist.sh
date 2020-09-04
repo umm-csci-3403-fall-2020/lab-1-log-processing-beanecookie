380f6f1e478115db5de958bebb7cc775e1245af7
Robert-Beane <beane031@morris.umn.edu>
2020-09-03 20:44:22 -0500

    Add awk filtering for only username

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,6 +5,7 @@ cd "$client" || exit
 
 #sort failed login data by username and put into new file
 sort -k 4 failed_login_data.txt > sorted_login_data.txt
+awk -F "[: ]+" '{print $4}' sorted_login_data.txt > sorted_user_data.txt
 
 #count the number of times a username is used
-uniq -f 3 -c sorted_login_data.txt > counted_login_data.txt
+uniq -c sorted_user_data.txt > counted_user_data.txt

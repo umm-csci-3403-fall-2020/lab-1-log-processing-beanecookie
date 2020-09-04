bbb4686f95637b61d176dcd350518a86e0c1525d
Robert-Beane <beane031@morris.umn.edu>
2020-09-04 11:03:41 -0500

    Clean create_username_dist.sh to remove abundent .txt files

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,14 +5,11 @@ client=$1
 cd "$client" || exit
 
 #sort failed login data by username and put into new file
-sort -k 4 */failed_login_data.txt > sorted_login_data.txt
-awk -F "[: ]+" '{print $4}' sorted_login_data.txt > sorted_user_data.txt
-
-#count the number of times a username is used
-uniq -c sorted_user_data.txt > counted_user_data.txt
-
-# putting usernames and # of tries into html
-awk '{print  "data.addRow([\x27"$2"\x27, "$1"]);"}' counted_user_data.txt > user_addrow.txt
+sort -k 4 */failed_login_data.txt | \
+    awk -F "[: ]+" '{print $4}' | \
+    uniq -c | \
+    # putting usernames and # of tries into html \
+    awk '{print  "data.addRow([\x27"$2"\x27, "$1"]);"}' > user_addrow.txt
 
 #wrap the data and create the html file
 "$here"/bin/wrap_contents.sh user_addrow.txt "$here"/html_components/username_dist username_dist.html

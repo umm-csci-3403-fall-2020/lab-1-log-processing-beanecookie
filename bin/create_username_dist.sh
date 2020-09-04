f532d9de3b078dd845085f017edf05af5311b34a
Cookie Vang <vang2660@morris.umn.edu>
2020-09-03 19:11:01 -0500

    Added command to wrap html file

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -8,3 +8,6 @@ sort -k 4 failed_login_data.txt > sorted_login_data.txt
 
 #count the number of times a username is used
 uniq -f 3 -c sorted_login_data.txt > counted_login_data.txt
+
+#wrap the data and create the html file
+#wrap_contents.sh counted_login_data.txt "$client" username_distribution.html

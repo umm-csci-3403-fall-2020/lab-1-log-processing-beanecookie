7a7702c2ccf53375bd4dbf0b22cb7de0299177cc
Cookie Vang <vang2660@morris.umn.edu>
2020-09-07 22:29:23 -0500

    Almost complete create country html file. One test left to pass

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+here=$(pwd)
+client=$1
+cd "$client" || exit
+
+#sorting the failed login data by IP address
+sort -k 5 -- */failed_login_data.txt | \
+	awk -F "[: ]+" '{print $5}' > sortedIp.txt
+
+#using join and awk to print country code and occurrence
+join sortedIp.txt etc/country_IP_map.txt | \
+	awk -F "[: }+" '{print $2}' | \
+	uniq -c | \
+	awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > country_addrow.txt
+
+#wrapping the contents into an html file
+"$here"/bin/wrap_contents.sh country_addrow.txt "$here"/html_components/country_dist country_dist.html

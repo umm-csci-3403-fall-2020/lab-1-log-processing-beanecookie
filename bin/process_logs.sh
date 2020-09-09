0cb198fcfb538008c6d31b62aadda04ae4dc23cc
Robert-Beane <beane031@morris.umn.edu>
2020-09-08 20:31:04 -0500

    Finish process_logs.sh so it passes all test and passes shellcheck.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
 here=$(pwd)
-clients=$@
 
 tmpdir=$(mktemp -d -t tmp_logs-XXX)
-#cd "$tmpdir" || exit
 
+
+# loop tomake seperate directories for all the machines with the extracted contents of each machine handed to it.
+# also runs process_client_logs on each set of secure files.
 for r in "$@"
 do
 	noExt=$(basename -s .tgz "$r")
 	mkdir "$tmpdir"/"$noExt"
        	tar -xzf "$r" -C "$tmpdir"/"$noExt"
+	bin/process_client_logs.sh "$tmpdir"/"$noExt"
 done
+
+# calls each .sh scripts created to make one final report
+bin/create_username_dist.sh "$tmpdir"
+bin/create_hours_dist.sh "$tmpdir"
+bin/create_country_dist.sh "$tmpdir"
+bin/assemble_report.sh "$tmpdir"
+
+mv "$tmpdir"/failed_login_summary.html "$here"
+
+rm -r "$tmpdir"
